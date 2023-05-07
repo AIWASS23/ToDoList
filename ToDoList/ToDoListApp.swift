53c1f41d2232e00fec257a26af2caedcd28a5ba6
AIWASS23 <marcelodearaujo677@gmail.com>
2023-05-07 09:28:47 -0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Marcelo De Araújo on 07/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
